c319a3783db2ce23c2c9536fe1bca6c86b101aa6
jian-lin <jian-lin@mail.chungyo.net>
2020-09-10 14:02:00 +0800

    laravel專案

---
+++ laravel/resources/views/testchild.blade.php
@@ -0,0 +1,15 @@
+
+
+@extends('layouts.test')
+
+@section('title','pagetitle')
+
+@section('sidebar')
+    @parent
+    
+    <p>測試中</p>
+@endsection
+
+@section('content')
+    <p>主要內容</p>
+@endsection
